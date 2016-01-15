f935f1017f9a365d406b6421cfacd8e2fb1af2c4
Albert Vives Peraita <albert.avipe@gmail.com>
2016-01-15 21:13:09 +0100

    Added assets and an arena scene to test some scripts

---
+++ Assets/Scripts/Damage.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Damage : MonoBehaviour
+{
+	
+	public float damageAmount = 10.0f;
+
+	public float continuousTimeBetweenHits = 0;
+
+	public GameObject explosionPrefab;
+
+	private float savedTime = 0;
+	private Move moveScript;
+	private ArrayList targets;
+	private GameObject target;
+
+	void Start()
+	{
+		moveScript = gameObject.GetComponent<Move> ();
+		targets = new ArrayList();
+	}
+
+	void Update()
+	{
+		if (targets.Count > 0) {
+			moveScript.StopMove ();
+			if (ValidTarget ()) {
+				AttackTarget ();
+			}
+		} else {
+			moveScript.ResumeMove();
+		}
+	}
+
+	private bool ValidTarget () {
+		ArrayList tempTargets = (ArrayList) targets.Clone();
+		foreach (GameObject g in tempTargets) {
+			if (g != null && g.GetComponent<Health> ().isAlive) {
+				target = g;
+				return true;
+			} else {
+				targets.Remove (g);
+			}
+		}
+		return false;
+	}
+
+	private void AttackTarget() {
+		if (Time.time - savedTime >= continuousTimeBetweenHits) {
+			savedTime = Time.time;
+			if (target.GetComponent<Health> () != null) {	// if the hit object has the Health script on it, deal damage
+				target.GetComponent<Health> ().ApplyDamage (damageAmount);
+
+				if (explosionPrefab != null) {
+					Instantiate (explosionPrefab, transform.position, transform.rotation);
+				}
+			}
+		}
+	}
+
+
+
+	void OnTriggerEnter2D (Collider2D collision)						// used for things like bullets, which are triggers.  
+	{
+//		if (this.tag == "PlayerBullet" && collision.gameObject.tag == "Player")	// if the player got hit with it's own bullets, ignore it
+//				return;
+		
+		if (collision.isTrigger || collision.gameObject.layer != 8)
+			return;
+
+		targets.Add(collision.gameObject);
+		moveScript.StopMove();
+
+	}
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	public float initialSpeed = 1;
+
+
+	private bool isMoving = true;
+	private float speed;
+
+	// Use this for initialization
+	void Start () {
+		speed = initialSpeed;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.Translate (Vector3.right * speed * Time.deltaTime);
+	}
+
+	public void StopMove(){
+		if (isMoving) {
+			speed = 0f;
+			isMoving = false;
+		}
+	}
+
+	public void ResumeMove() {
+		if (!isMoving) {
+			speed = initialSpeed;
+			isMoving = true;
+		}
+	}
+}
